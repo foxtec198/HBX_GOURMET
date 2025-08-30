fa389b5b925971b0dba6897615d51330814a1ba3
Guilherme Breve <64221923+foxtec198@users.noreply.github.com>
2025-08-29 22:39:38 -0300

    .

--- js/cardapio.js
+++ js/cardapio.js
@@ -44,6 +44,11 @@ function change_theme(){
 
 }
 
+function real(str){ // Converte STR/INT em Moeda R$
+    str = parseFloat(str)
+    return str.toLocaleString('pt-br', {style:'currency', currency:'BRL'})
+}
+
 async function get_prods(){
     const req = await request('produtos')
     const res = await req.json()
@@ -55,71 +60,114 @@ async function get_prods(){
         res.forEach(item => {
             ctgs.push(item.categoria)
         })
-    }
-    categorias = new Set(ctgs)
-    categorias.forEach(item => {
-        const li = document.createElement('li')
-        li.classList.add('nav-item')
 
-        const a = document.createElement('a')
-        a.classList.add('nav-link')
-        a.href = `#${item}`
-        a.textContent = item
-        a.addEventListener('click', function(){
-            // Seta o atual como ativo
-            this.classList.add('active', 'fw-bold')
-            // Desativa os demais
-            document.querySelectorAll('.nav-link').forEach(items =>{
-                items.classList.remove('active', 'fw-bold')
+        categorias = new Set(ctgs)
+        categorias.forEach(item => {
+            const li = document.createElement('li')
+            li.classList.add('nav-item')
+    
+            const a = document.createElement('a')
+            a.classList.add('nav-link')
+            a.href = `#${item}`
+            a.textContent = item
+            a.addEventListener('click', function(){
+                // Seta o atual como ativo
+                this.classList.add('active', 'fw-bold')
+                // Desativa os demais
+                document.querySelectorAll('.nav-link').forEach(items =>{
+                    items.classList.remove('active', 'fw-bold')
+                })
             })
+    
+            li.appendChild(a)
+            list_ctg.appendChild(li)
+    
+            const ac_item = document.createElement('div')
+            ac_item.classList.add('accordion-item')
+            ac_item.id = item
+    
+            const ac_header = document.createElement('h2')
+            ac_header.classList.add('accordion-header')
+    
+            const ac_btn = document.createElement('button')
+            ac_btn.classList.add('accordion-button', 'fw-bold')
+            ac_btn.type = 'button'
+            ac_btn.setAttribute('data-bs-toggle', 'collapse')
+            ac_btn.setAttribute('data-bs-target', `#ac_${item}`)
+            ac_btn.textContent = item
+    
+            ac_header.appendChild(ac_btn)
+    
+            const ac = document.createElement('div')
+            ac.classList.add('accordion-colapse', 'collapse', 'show')
+            ac.id = `ac_${item}`
+    
+            const ac_body = document.createElement('div')
+            ac_body.classList.add('accordion-body', 'd-flex', 'flex-wrap', 'p-2', 'gap-2')
+            ac_body.id = `ac_body_${item}`
+    
+            ac.appendChild(ac_body)
+    
+            ac_item.appendChild(ac_header)
+            ac_item.appendChild(ac)
+    
+            ctgs_ls.appendChild(ac_item)
         })
+    
+        
+        res.forEach(item => {
+            if(item.valor > 0){
+                const dvProd = document.createElement('div')
+                dvProd.classList.add('card', 'd-flex', 'flex-grow-1')
+                dvProd.style.flexBasis = '300px'
+                dvProd.style.maxWidth = '330px'
+    
+                const cardH = document.createElement('div')
+                cardH.classList.add('card-header')
+                if(item.quantidade <= 0){
+                    cardH.innerHTML = `
+                    <div class='d-flex justify-content-between'>
+                        <span>${item.nome}</span>
+                        <span class='badge text-bg-danger'>Sem estoque!</span>
+                    </div>
+                    `
+                }else{
+                    cardH.textContent = item.nome
+                }
 
-        li.appendChild(a)
-        list_ctg.appendChild(li)
+                const cardB = document.createElement('div')
+                cardB.classList.add('card-body', 'd-flex', 'justify-content-center')
+                const img = document.createElement('img')
+                img.classList.add('img-fluid', 'img-thumbnail')
+                img.style.objectFit = 'cover'
+                img.style.width = '200px'
+                img.style.height = '200px'
+                if(item.img !== 'blank.png'){img.src = server + 'img/gourmet/' + item.img}
+                else{img.src = server + 'img/blank.png'}
+                if(item.quantidade <= 0){img.style.filter = 'grayscale(100%)'}
+                cardB.appendChild(img)
 
-        const ac_item = document.createElement('div')
-        ac_item.classList.add('accordion-item')
-        ac_item.id = item
+                const cardF = document.createElement('div')
+                cardF.classList.add('card-header')
 
-        const ac_header = document.createElement('h2')
-        ac_header.classList.add('accordion-header')
+                const spNome = document.createElement('span')
+                spNome.classList.add('fs-3', 'fw-bold')
+                spNome.textContent = real(item.valor)
+                
+                cardF.appendChild(spNome)
+                
+                dvProd.appendChild(cardH)
+                dvProd.appendChild(cardB)
+                dvProd.appendChild(cardF)
+                
+                document.getElementById(`ac_body_${item.categoria}`).appendChild(dvProd)
+            }
+        })
+    }
+    
 
-        const ac_btn = document.createElement('button')
-        ac_btn.classList.add('accordion-button', 'fw-bold')
-        ac_btn.type = 'button'
-        ac_btn.setAttribute('data-bs-toggle', 'collapse')
-        ac_btn.setAttribute('data-bs-target', `#ac_${item}`)
-        ac_btn.textContent = item
-
-        ac_header.appendChild(ac_btn)
-
-        const ac = document.createElement('div')
-        ac.classList.add('accordion-colapse', 'collapse', 'show')
-        ac.id = `ac_${item}`
-
-        const ac_body = document.createElement('div')
-        ac_body.classList.add('accordion-body', 'd-flex', 'flex-wrap', 'p-2', 'gap-2')
-
-        ac.appendChild(ac_body)
-
-        ac_item.appendChild(ac_header)
-        ac_item.appendChild(ac)
-
-        ctgs_ls.appendChild(ac_item)
-    })
-
-    const dvProd = document.createElement('div')
-    dvProd.classList.add('card')
-
-    const cardH = document.createElement('div')
-    cardH.classList.add('card-header')
-
+    
 
 }
-
-
-
-
-
 
 get_prods()
